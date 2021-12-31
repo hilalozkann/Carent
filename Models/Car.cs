f83f7454da7a1dbfc4dc78d2445b4587137408f5
Hilal ÖZKAN <48198674+hilalozkann@users.noreply.github.com>
2021-12-31 21:29:06 +0300

    Add files via upload

---
+++ Models/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace RentProject.Models
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+
+        public string CarOwnerID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Brand { get; set; }
+
+        public string CarModel { get; set; }
+
+        public byte[] CarImage { get; set; }
+        public int Year { get; set; }
+
+        public int Km { get; set; }
+
+        public string CarAddress { get; set; }
+
+        public string FuelType { get; set; }
+
+        public string GearType { get; set; }
+
+        public int MotorPower { get; set; }
+
+
+    }
+}
